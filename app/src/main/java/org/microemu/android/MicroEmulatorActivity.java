05de460218016655f0bcea8ed6d48863071677b9
Александр Мельников <melnikov.a@office.lan>
2022-05-12 20:52:10 +0300

    refactoring

--- app/src/main/java/org/microemu/android/MicroEmulatorActivity.java
+++ app/src/main/java/org/microemu/android/MicroEmulatorActivity.java
@@ -1,42 +1,44 @@
 /**
- *  MicroEmulator
- *  Copyright (C) 2009 Bartek Teodorczyk <barteo@barteo.net>
+ * MicroEmulator
+ * Copyright (C) 2009 Bartek Teodorczyk <barteo@barteo.net>
+ * <p>
+ * It is licensed under the following two licenses as alternatives:
+ * 1. GNU Lesser General Public License (the "LGPL") version 2.1 or any newer version
+ * 2. Apache License (the "AL") Version 2.0
+ * <p>
+ * You may not use this file except in compliance with at least one of
+ * the above two licenses.
+ * <p>
+ * You may obtain a copy of the LGPL at
+ * http://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt
+ * <p>
+ * You may obtain a copy of the AL at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the LGPL or the AL for the specific language governing permissions and
+ * limitations.
  *
- *  It is licensed under the following two licenses as alternatives:
- *    1. GNU Lesser General Public License (the "LGPL") version 2.1 or any newer version
- *    2. Apache License (the "AL") Version 2.0
- *
- *  You may not use this file except in compliance with at least one of
- *  the above two licenses.
- *
- *  You may obtain a copy of the LGPL at
- *      http://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt
- *
- *  You may obtain a copy of the AL at
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the LGPL or the AL for the specific language governing permissions and
- *  limitations.
- *
- *  @version $Id: MicroEmulatorActivity.java 1918 2009-01-21 12:56:43Z barteo $
+ * @version $Id: MicroEmulatorActivity.java 1918 2009-01-21 12:56:43Z barteo $
  */
 
 package org.microemu.android;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-
-import javax.microedition.io.ConnectionNotFoundException;
-import javax.microedition.lcdui.Canvas;
-import javax.microedition.lcdui.Displayable;
-
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
-import jimm.modules.DebugLog;
+import android.view.View;
+
 import org.microemu.DisplayAccess;
 import org.microemu.DisplayComponent;
 import org.microemu.MIDletAccess;
@@ -54,89 +56,82 @@ import org.microemu.device.EmulatorContext;
 import org.microemu.device.FontManager;
 import org.microemu.device.InputMethod;
 
-import android.app.Activity;
-import android.app.Dialog;
-import android.content.ActivityNotFoundException;
-import android.content.Intent;
-import android.content.res.Configuration;
-import android.content.res.TypedArray;
-import android.graphics.drawable.Drawable;
-import android.net.Uri;
-import android.os.Bundle;
-import android.os.Handler;
-import android.util.Log;
-import android.view.Display;
-import android.view.View;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+
+import javax.microedition.io.ConnectionNotFoundException;
+import javax.microedition.lcdui.Canvas;
+import javax.microedition.lcdui.Displayable;
+
+import jimm.modules.DebugLog;
 
 public abstract class MicroEmulatorActivity extends Activity {
-		
-	public static AndroidConfig config = new AndroidConfig();
-	
-	public boolean windowFullscreen;
 
-	private Handler handler = new Handler();
-	
-	private Thread activityThread;
-	
-	protected View contentView;
+    public static AndroidConfig config = new AndroidConfig();
 
-	private Dialog dialog;
-	
-	private ArrayList<ActivityResultListener> activityResultListeners = new ArrayList<ActivityResultListener>();
-	
-	protected EmulatorContext emulatorContext;
-	
-	public void setConfig(AndroidConfig config) {
-		MicroEmulatorActivity.config = config;
-	}
-    
+    public boolean windowFullscreen;
+
+    private final Handler handler = new Handler();
+
+    private Thread activityThread;
+
+    protected View contentView;
+
+    private Dialog dialog;
+
+    private final ArrayList<ActivityResultListener> activityResultListeners = new ArrayList<>();
+
+    protected EmulatorContext emulatorContext;
+
+    public void setConfig(AndroidConfig config) {
+        MicroEmulatorActivity.config = config;
+    }
+
     public EmulatorContext getEmulatorContext() {
         return emulatorContext;
     }
 
-	public boolean post(final Runnable r) {
-        Runnable nn = new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    r.run();
-                } catch (Exception e) {
-                    DebugLog.panic("err ", e);
-                }
+    public boolean post(final Runnable r) {
+        Runnable nn = () -> {
+            try {
+                r.run();
+            } catch (Exception e) {
+                DebugLog.panic("err ", e);
             }
         };
-		if (activityThread == Thread.currentThread()) {
-			nn.run();
-			return true;
-		} else {
-			return handler.post(nn);
-		}
-	}
-	
-	public boolean postDelayed(Runnable r, long delayMillis) {
-		if (activityThread == Thread.currentThread()) {
-			r.run();
-			return true;
-		} else {
-			return handler.postDelayed(r, delayMillis);
-		}
-	}
-	
-	public boolean isActivityThread() {
-		return (activityThread == Thread.currentThread());
-	}
+        if (activityThread == Thread.currentThread()) {
+            nn.run();
+            return true;
+        } else {
+            return handler.post(nn);
+        }
+    }
 
-	@Override
-	protected void onCreate(Bundle icicle) {
-		super.onCreate(icicle);
-		
+    public boolean postDelayed(Runnable r, long delayMillis) {
+        if (activityThread == Thread.currentThread()) {
+            r.run();
+            return true;
+        } else {
+            return handler.postDelayed(r, delayMillis);
+        }
+    }
+
+    public boolean isActivityThread() {
+        return (activityThread == Thread.currentThread());
+    }
+
+    @Override
+    protected void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+
         emulatorContext = new EmulatorContext() {
 
-            private InputMethod inputMethod = new AndroidInputMethod();
+            private final InputMethod inputMethod = new AndroidInputMethod();
 
-            private DeviceDisplay deviceDisplay = new AndroidDeviceDisplay(MicroEmulatorActivity.this, this, 200, 200);
-            
-            private FontManager fontManager = new AndroidFontManager(getResources().getDisplayMetrics());
+            private final DeviceDisplay deviceDisplay = new AndroidDeviceDisplay(MicroEmulatorActivity.this, this, 200, 200);
+
+            private final FontManager fontManager = new AndroidFontManager(getResources().getDisplayMetrics());
 
             public DisplayComponent getDisplayComponent() {
                 // TODO consider removal of EmulatorContext.getDisplayComponent()
@@ -165,7 +160,7 @@ public abstract class MicroEmulatorActivity extends Activity {
                         if (p == null) {
                             return MicroEmulatorActivity.this.getAssets().open(name);
                         } else {
-                        	String folder = origClass.getPackage().getName().replace('.', '/');
+                            String folder = origClass.getPackage().getName().replace('.', '/');
                             return MicroEmulatorActivity.this.getAssets().open(folder + "/" + name);
                         }
                     }
@@ -175,8 +170,7 @@ public abstract class MicroEmulatorActivity extends Activity {
                 }
             }
 
-            public boolean platformRequest(String url) throws ConnectionNotFoundException 
-            {
+            public boolean platformRequest(String url) throws ConnectionNotFoundException {
                 try {
                     startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
                 } catch (ActivityNotFoundException e) {
@@ -185,59 +179,59 @@ public abstract class MicroEmulatorActivity extends Activity {
 
                 return true;
             }
-                    
+
         };
-		
-		activityThread = Thread.currentThread();
-	}
-	
-	@Override
-	public void setContentView(View view) {
-Log.d("AndroidCanvasUI", "set content view: " + view);
-		super.setContentView(view);
-		contentView = view;
-	}
-		
-	@Override
-	public void onConfigurationChanged(Configuration newConfig) {
-		super.onConfigurationChanged(newConfig);
-		
-		MIDletAccess ma = MIDletBridge.getMIDletAccess();
-		if (ma == null) {
-			return;
-		}
-		DisplayAccess da = ma.getDisplayAccess();
+
+        activityThread = Thread.currentThread();
+    }
+
+    @Override
+    public void setContentView(View view) {
+        Log.d("AndroidCanvasUI", "set content view: " + view);
+        super.setContentView(view);
+        contentView = view;
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        MIDletAccess ma = MIDletBridge.getMIDletAccess();
+        if (ma == null) {
+            return;
+        }
+        DisplayAccess da = ma.getDisplayAccess();
         if (da != null) {
             Displayable d = da.getCurrent();
             if (d instanceof Canvas) {
-                CanvasView c = ((AndroidCanvasUI)d.getUi()).getCanvasView();
+                CanvasView c = ((AndroidCanvasUI) d.getUi()).getCanvasView();
                 AndroidDeviceDisplay deviceDisplay = (AndroidDeviceDisplay) DeviceFactory.getDevice().getDeviceDisplay();
                 deviceDisplay.displayRectangleWidth = c.getWidth();
                 deviceDisplay.displayRectangleHeight = c.getHeight();
                 da.sizeChanged();
                 deviceDisplay.repaint(0, 0, deviceDisplay.getFullWidth(), deviceDisplay.getFullHeight());
             }
-		}
-	}
-	
-	public void addActivityResultListener(ActivityResultListener listener) {
-		activityResultListeners.add(listener);
-	}
-	
-	public void removeActivityResultListener(ActivityResultListener listener) {
-		activityResultListeners.remove(listener);
-	}
-	
-	@Override
-	protected final void onActivityResult(int requestCode, int resultCode, Intent data) {
+        }
+    }
+
+    public void addActivityResultListener(ActivityResultListener listener) {
+        activityResultListeners.add(listener);
+    }
+
+    public void removeActivityResultListener(ActivityResultListener listener) {
+        activityResultListeners.remove(listener);
+    }
+
+    @Override
+    protected final void onActivityResult(int requestCode, int resultCode, Intent data) {
         for (ActivityResultListener activityResultListener : activityResultListeners) {
             if (activityResultListener.onActivityResult(requestCode, resultCode, data)) {
                 return;
             }
         }
-		
-		super.onActivityResult(requestCode, resultCode, data);
-	}
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 
     @Override
     public boolean onTrackballEvent(MotionEvent event) {
